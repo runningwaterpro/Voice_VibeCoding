983eb1a510c3fa8dd1c3ce51d8bb362657e6105a
runningwaterpro <runningwaterpro@gmail.com>
2026-09-25 17:08:59 +0800

    fix: quote Windows manifest linker argument

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,7 +5,7 @@ fn main() {
     // otherwise fails before the first test with STATUS_ENTRYPOINT_NOT_FOUND.
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
         println!(
-            "cargo:rustc-link-arg=/MANIFESTDEPENDENCY:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\""
+            "cargo:rustc-link-arg=\"/MANIFESTDEPENDENCY:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\""
         );
     }
     tauri_build::build()
